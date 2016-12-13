9dd34e2c31e2005e20e286d0446a26a48962cced
DESKTOP-C5KEOL7\Max <marad747@yandex.ua>
2016-12-13 11:40:55 +0200

    add in - app purchasing

--- Scripts/level1/HeroControlled.cs
+++ Scripts/level1/HeroControlled.cs
@@ -8,10 +8,11 @@ public class HeroControlled : MonoBehaviour {
     // Use this for initialization
     public Rigidbody2D rb;
     public static HeroControlled instance;
-    public GameObject Hero, currentBlock, previousBlock;
+    public GameObject Hero, currentBlock, previousBlock,sliderObject;
     public Animator anim;
     public float startTime, forceRight, forceUp, diff, cunt;
     public bool press;
+    public UISlider slider;
 
     void Awake () {
         instance = this;
@@ -22,24 +23,29 @@ public class HeroControlled : MonoBehaviour {
         if (currentBlock == previousBlock) {
             Level1Manager.instance.GameOver();            
         }
-        if (press)
+        if (press) {
             diff = Time.time - startTime;
+            slider.value = diff/2;
+            if (diff > 2) {
+                diff = 0;
+                startTime = Time.time;
+            }
+        }
     }    
 
     void OnMouseDown() {
         startTime = Time.time;
         anim.SetBool("jump",true);
         press = true;
+        sliderObject.SetActive(true);
     }
 
-    void OnMouseUp() {        
+    void OnMouseUp() {
+        sliderObject.SetActive(false);
         if (diff <= 2f) {
             forceUp = 200 * diff;
             forceRight = 170 * diff;
-        } else {
-            forceUp = Random.Range(320f,350f);
-            forceRight = Random.Range(250f,280f);
-        } 
+        }
         if (HeroCollision.instance.isGround == true && diff >= 0.1f) {
             anim.SetBool("fly",true);
             rb.AddForce(Hero.transform.up * forceUp);
---
+++ Scripts/level1/adsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class adsController : MonoBehaviour {
+
+    // Use this for initialization
+    public static adsController instance;
+    public string id;
+    BannerView bannerView;
+    void Awake () {
+        instance = this;
+        CreateBanner();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void CreateBanner() {
+        bannerView = new BannerView(id,AdSize.Banner,AdPosition.Top);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("1ed90b7345391805").Build();
+        // Load the banner with the request.
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+        bannerView.LoadAd(request);
+        
+    }
+
+    public void HandleOnAdLoaded(object sender,EventArgs args) {
+        bannerView.Show();        
+    }
+}
