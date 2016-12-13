5463776e1ff29fd58ea0d2c58f0c51781082d757
DESKTOP-C5KEOL7\Max <marad747@yandex.ua>
2016-12-13 14:16:42 +0200

    ревард видео(не работает )

--- Scripts/level1/adsController.cs
+++ Scripts/level1/adsController.cs
@@ -7,12 +7,14 @@ using System;
 public class adsController : MonoBehaviour {
 
     // Use this for initialization
-    public static adsController instance;
-    public string id;
+    //public static adsController instance;
+    public string id, idrewardBasedVideo;
     BannerView bannerView;
+    RewardBasedVideoAd rewardBasedVideo;
     void Awake () {
-        instance = this;
+        //instance = this;
         CreateBanner();
+        CreateRevardVideo();
     }
 	
 	// Update is called once per frame
@@ -20,7 +22,7 @@ public class adsController : MonoBehaviour {
 		
 	}
     public void CreateBanner() {
-        bannerView = new BannerView(id,AdSize.Banner,AdPosition.Top);
+        bannerView = new BannerView(id,AdSize.Banner,AdPosition.TopRight);
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("1ed90b7345391805").Build();
         // Load the banner with the request.
@@ -32,4 +34,27 @@ public class adsController : MonoBehaviour {
     public void HandleOnAdLoaded(object sender,EventArgs args) {
         bannerView.Show();        
     }
+
+    public void CreateRevardVideo() {
+
+        rewardBasedVideo = RewardBasedVideoAd.Instance;
+        AdRequest request = new AdRequest.Builder().Build();
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        rewardBasedVideo.LoadAd(request,id);
+
+    }
+
+    public void PlayReklamy() {
+        if (rewardBasedVideo.IsLoaded()) {
+            rewardBasedVideo.Show();
+        }
+    }
+    
+    public void HandleRewardBasedVideoRewarded(object sender,Reward args) {      
+        PlayerPrefs.SetInt("CountCoins",PlayerPrefs.GetInt("CountCoins") + 250);
+    }
+    void OnGUI() {
+        GUILayout.Label(rewardBasedVideo.IsLoaded().ToString());
+    }
+
 }
